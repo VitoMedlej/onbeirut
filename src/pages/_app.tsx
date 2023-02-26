ced3c287d9320339005e5dea68841cdee52f8abf
vito medlej <vito.medlej@gmail.com>
2023-02-26 13:35:03 +0300

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,23 @@ import '@/styles/styles.css'
 import type { AppProps } from 'next/app';
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import gsap from 'gsap'
+import Script from 'next/script';
 
 gsap.registerPlugin(ScrollTrigger);
 export default function App({ Component, pageProps }: AppProps) {
   
-  return <Component {...pageProps} />
+  return <>
+    {/* <!-- Google tag (gtag.js) --> */}
+<Script strategy="lazyOnload"  async src="https://www.googletagmanager.com/gtag/js?id=G-6FMZ4XFZ6X"></Script>
+<Script strategy="lazyOnload">
+  {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6FMZ4XFZ6X');
+  `}
+</Script>
+  <Component {...pageProps} />
+  </>
 }
